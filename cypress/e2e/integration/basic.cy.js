5b52773c2234152c2aa78429e5cbd04f8a62cdec
graciela.gallas <graciela.gallas@onpier.de>
2025-08-20 15:28:27 +0200

    added helper last touch

--- cypress/e2e/integration/basic.cy.js
+++ cypress/e2e/integration/basic.cy.js
@@ -15,11 +15,25 @@ describe("Cypress basics", ()=>{
         //     console.log(title)
         // })   
 
-        cy.title().should(title => {
+        let syncTitle
+        cy.title().then(title => {
             console.log(title)
+
+        cy.get('#formNome').type(title)
+
+        syncTitle = title
         }) 
+
+
+        cy.get('[data-cy="dataSobrenome"]').then($el =>{
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el =>{
+            cy.wrap($el).type(syncTitle)
+        })
         
-        //TODO escrever o titulo no campo de texto
+       
         
     })
 
