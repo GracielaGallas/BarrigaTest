372445d623041d1d74e5ee36e88af6b03af0e89c
graciela.gallas <graciela.gallas@onpier.de>
2025-08-23 09:55:37 +0200

    add/feat_tick

--- cypress/e2e/integration/time.cy.js
+++ cypress/e2e/integration/time.cy.js
@@ -16,4 +16,26 @@ describe("Work with alerts", ()=> {
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain', '04/12/1977')
     })
+
+    it.only('Goes to the future', ()=>{
+        // cy.get('#buttonTimePassed').click()
+        // //cy.get('#resultado > span').invoke('text').then(Number).should('gt',1755934715181)
+        // cy.get('#resultado > span').invoke('text').should('gt','1755934715181')
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(Number).should('lte',0)
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').then(Number).should('lte',1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(Number).should('gte',5000)
+
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(Number).should('gte',15000)
+
+    })
 })    
