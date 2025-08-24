4cddd0127c8ad1ff73fe84f192392f8efb7997a7
graciela.gallas <graciela.gallas@onpier.de>
2025-08-24 18:34:11 +0200

    test/backend_create_new_account

--- cypress/e2e/integration/barriga/functional.cy.js
+++ cypress/e2e/integration/barriga/functional.cy.js
@@ -4,22 +4,22 @@ import loc from '../../../support/locators'
 import '../../../support/commandsAccount'
 
 describe("all functional tests", () => {
-  beforeEach(() => {
-     cy.fixture('barrigaData').then((dados) => {
-      cy.login(dados.email, dados.psw)
-     })
-  })
- 
-    it('Should create a new account', ()=>{
+    beforeEach(() => {
+        cy.fixture('barrigaData').then((dados) => {
+            cy.login(dados.email, dados.psw)
+        })
+    })
+
+    it('Should create a new account', () => {
         cy.fixture('barrigaData').then((dados) => {
             cy.accountsAccess()
             cy.insertAccount(dados.new_account)
             cy.get(loc.MESSAGE).should('contain', dados.msg_sucessful)
-    
-        }) 
-    }) 
 
-    it('Should change an account', ()=>{
+        })
+    })
+
+    it('Should change an account', () => {
         cy.fixture('barrigaData').then((dados) => {
             cy.accountsAccess()
             cy.contains('td', dados.new_account).parent('tr').find('i.fa-edit').click()
@@ -27,19 +27,19 @@ describe("all functional tests", () => {
             cy.get(loc.ACCOUNTS_PAGE.BTN_SAVE).click()
             cy.get(loc.MESSAGE).should('contain', dados.msg_sucessful)
 
-         })
+        })
     })
 
-    it('Should try to add account with the same name', ()=>{
-            cy.fixture('barrigaData').then((dados) => {
-                cy.accountsAccess()
-                cy.insertAccount(dados.transaction_account)
-                cy.get(loc.MESSAGE).should('contain', dados.msg_error)
-            })
-    
+    it('Should try to add account with the same name', () => {
+        cy.fixture('barrigaData').then((dados) => {
+            cy.accountsAccess()
+            cy.insertAccount(dados.transaction_account)
+            cy.get(loc.MESSAGE).should('contain', dados.msg_error)
+        })
+
     })
 
-    it('Should add a transaction',()=>{
+    it('Should add a transaction', () => {
         cy.fixture('barrigaData').then((dados) => {
             cy.get(loc.MENU.TRANSACTION).click()
             cy.get(loc.TRANSACTION_PAGE.T_DESCRIPTION).type(dados.transaction_description)
@@ -51,37 +51,37 @@ describe("all functional tests", () => {
             cy.get(loc.MESSAGE).should('contain', dados.msg_sucessful)
             cy.get(loc.TRANSACTION_PAGE.T_TABLE).contains(dados.transaction_description)
                 .parents('[data-test="mov-row"]')
-                .should('contain', dados.transaction_description) 
-        })
-    })    
-
-    it('Should get balance',()=>{
-        cy.fixture('barrigaData').then((dados) => {
-            cy.get(loc.MENU.HOME).click()
-            cy.contains('td', dados.transaction_account)
-                .next()                                     
-                .invoke('text').then(t => t.replace(/\u00a0/g, ' ').trim())
-                .should('contain', 'R$ 5.000,00') 
+                .should('contain', dados.transaction_description)
         })
     })
 
-    it('Should delete a transaction', ()=>{
+    it('Should get balance', () => {
+        cy.fixture('barrigaData').then((dados) => {
+            cy.get(loc.MENU.HOME).click()
+            cy.contains('td', dados.transaction_account)
+                .next()
+                .invoke('text').then(t => t.replace(/\u00a0/g, ' ').trim())
+                .should('contain', 'R$ 5.000,00')
+        })
+    })
+
+    it('Should delete a transaction', () => {
         cy.fixture('barrigaData').then((dados) => {
             cy.get(loc.MENU.BALANCE).click()
             cy.contains('[data-test="mov-row"]', dados.transaction_description)
                 .find('i.fa-trash-alt')
                 .click()
-            cy.get(loc.MESSAGE).should('contain', dados.msg_sucessful)    
-            })
-            
+            cy.get(loc.MESSAGE).should('contain', dados.msg_sucessful)
+        })
+
     })
 
-    it('should reset database',()=>{
-        cy.resetApp() 
+    it('should reset database', () => {
+        cy.resetApp()
     })
-    
-    
+
+
 })
 
 
-         
+
